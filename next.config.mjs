19483b600c61ea8141dea3bdd92a89e95ed302f3
Thomas Musselman <tmusselman@gmail.com>
2026-05-26 20:13:12 -0600

    set up config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Tells Next.js to build a static HTML export in an "out" folder
+  output: 'export',
+  
+  // Required if you plan to use the <Image /> component, as GitHub Pages 
+  // doesn't have an image optimization server.
+  images: {
+    unoptimized: true,
+  },
+
+  // NOTE: If your GitHub repo is named something like "username.github.io/my-project",
+  // you must uncomment the line below and change it to your repo name so CSS/JS assets load correctly.
+  // basePath: '/my-project',
 };
 
 export default nextConfig;
